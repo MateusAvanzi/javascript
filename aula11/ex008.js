9025b819b10503d050b2f837f454498029039b81
MateusAvanzi <113141693+MateusAvanzi@users.noreply.github.com>
2023-03-09 16:47:51 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var veloc = 60
+console.log(`A velocidade de seu carro é: ${veloc}Km/h`)
+if (veloc > 90) {
+    console.log(`Vooce ultrapassou a velocidadde permitida. MULTADO`)
+}
+console.log(`Use cinto de segurança`)
+
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+    if (pais == 'Brasil') {
+        console.log('Br')
+    } else {
+        console.log('Gringo')
+    }
