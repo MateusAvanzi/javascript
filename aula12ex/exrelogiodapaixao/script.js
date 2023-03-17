f83c624452299d52e40f05687f734cef10f07ef2
MateusAvanzi <113141693+MateusAvanzi@users.noreply.github.com>
2023-03-17 18:44:53 -0300

    ex03

---
+++ aula12ex/exrelogiodapaixao/script.js
@@ -0,0 +1,119 @@
+function carregar() {
+
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+   var data = new Date()
+    //var hora = data.getHours()
+var hora = 20
+
+  
+ 
+ 
+    if (hora >= 19 && hora < 20) {
+        img.src = 'foto19h.png'
+        msg.innerHTML = '“Jesus, ensinando sobre o lava pés, disse que era uma purificação das faltas quotidianas, porque os pés, caminhando descuidadamente na terra, se sujavam continuamente. Esse banho dos pés era espiritual e uma espécie de absolvição. Pedro, porém, viu nele apenas uma humilhação muito grande para o Mestre; não sabia que Jesus, para salvá-lo e aos outros homens, se humilharia na manhã seguinte até a morte de cruz.” (Anna Catarina Emmerich, “Vida e Paixão do Cordeiro de Deus”, Minha Biblioteca Católica, 129)'
+
+
+    }   else if (hora >= 20 && hora < 21) {
+        img.src = 'foto20h.png'
+        msg.innerHTML = 'Não me lembro de ter visto o Senhor comer as espécies consagradas, a não ser que eu não reparasse. Dando o Santíssimo Sacramento, deu-se de modo que parecia sair de si mesmo e derramar-se nos Apóstolos, em uma efusão amor misericordioso. (Anna Catarina Emmerich, “Vida e Paixão do Cordeiro de Deus”, Minha Biblioteca Católica, 134)'
+
+    } else if(hora >= 21 && hora < 22) {
+        img.src = 'foto21h.png'
+        
+
+    } else if(hora >= 22 && hora < 23) {
+        img.src = 'foto22h.png'
+        
+
+    } else if(hora >= 23 && hora < 0) {
+        img.src = 'foto23h.png'
+        
+
+    }
+    else if(hora >= 0 && hora < 1) {
+        img.src = '0h.png'
+        
+
+    }
+    else if(hora >= 1 && hora < 2) {
+        img.src = '1h.png'
+        
+
+    }
+    else if(hora >= 2 && hora < 3) {
+        img.src = 'foto2h.png'
+        
+
+    }
+    else if(hora >= 3 && hora < 4) {
+        img.src = 'foto3h.png'
+        
+
+    }   else if(hora >= 4 && hora < 5) {
+        img.src = 'foto4h.png'
+      
+
+    }
+    else if(hora >= 5 && hora < 6 ) {
+        img.src = 'foto5h.png'
+   
+
+    }
+    else if(hora >= 6 && hora < 7) {
+        img.src = 'foto6h.png'
+  
+    }
+    else if(hora >= 7 && hora < 8) {
+        img.src = 'foto7h.png'
+   
+
+    }
+    else if(hora >= 8 && hora < 9) {
+        img.src = 'foto8h.png'
+
+    }   
+      else if(hora >= 9 && hora < 10) {
+        img.src = 'foto9h.png'
+      
+
+    } else if(hora >= 10 && hora < 11) {
+
+
+    }
+    else if(hora >= 11 && hora < 12) {
+        img.src = 'foto11h.png'
+
+
+    }
+    else if(hora >= 12 && hora < 13) {
+        img.src = 'foto12h.png'
+
+
+    }
+    else if(hora >= 13 && hora < 14) {
+        img.src = 'foto13h.png'
+
+
+    }
+    else if(hora >= 14 && hora < 15) {
+        img.src = 'foto14h.png'
+
+
+    }  else if(hora >= 15 && hora < 16) {
+        img.src = 'foto15h.png'
+
+
+    }  else if(hora >= 16 && hora < 17) {
+        img.src = 'foto16h.png'
+
+
+    }  else if(hora >= 17 && hora < 18) {
+        img.src = 'foto17h.png'
+     
+
+    }
+
+}
+
+
